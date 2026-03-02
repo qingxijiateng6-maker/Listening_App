839f6867059ddac6ceef5080212f61f19c3b37d2
qingxijiateng6-maker <qingxijiateng6@gmail.com>
2026-03-02 20:35:30 +0900

    説明ページのurlを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,19 @@ export default function Home() {
         <VideoRegistrationForm />
       </section>
       <HistoryNavigation />
+      <section className="homeAppDescriptionSection" aria-labelledby="app-description-heading">
+        <p id="app-description-heading" className="homeAppDescriptionLabel">
+          このアプリの説明↓
+        </p>
+        <a
+          className="homeAppDescriptionLink"
+          href="https://peppered-chicory-bac.notion.site/3172738ccd0f80f795b7eaa1663c11fa?pvs=143"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://peppered-chicory-bac.notion.site/3172738ccd0f80f795b7eaa1663c11fa?pvs=143
+        </a>
+      </section>
     </main>
   );
 }
